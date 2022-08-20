ddbfe00b948519081b9e3cfcb11710665896fd55
Zhenying Yu <zhenyingyu6@gmail.com>
2022-08-20 19:43:48 -0400

    Update index.js

--- Code/data/index.js
+++ Code/data/index.js
@@ -1,12 +1,10 @@
 const commentsData = require('./comments');
-const testData = require('./test');
 const customersData = require('./customers');
 const reviewsData = require('./reviews');
 const restaurantsData = require('./restaurants');
 
 module.exports = {
-    comments: commentsData,
-    test: testData,
+    comments: commentsData,   
     customers: customersData,
     reviews: reviewsData,
     restaurants: restaurantsData
