16f46ca42c1cf306763e6c54596b59af5649b4a8
Zhenying Yu <zhenyingyu6@gmail.com>
2022-08-22 19:33:10 -0400

    Update seed.js
    fix the bug of submitting reviews

--- Code/tasks/seed.js
+++ Code/tasks/seed.js
@@ -1,8 +1,6 @@
 const connection = require('../config/mongoConnection');
 const data = require("../data/");
-const commentsData = data.comments;
 const customersData = data.customers;
-const reviewsData = data.reviews;
 const restaurantsData = data.restaurants;
 
 async function main() {
@@ -48,51 +46,8 @@ async function main() {
     const user2 = await customersData.createUser("Zhenying", "Yu", "zhenyingyu6@gmail.com", "yzyolala", "qawsed", "", "28");
     console.log("User 2 created successfully");
 
-    // const user3 = await customersData.createUser("Tonny", "Keith", "tonny@gmail.com", "tonnykk", "tonnykeith@123", "", "23");
-    // console.log("User 3 created successfully");
-
-    // const user4 = await customersData.createUser("Richard", "Endrew", "richard@gmail.com", "richardend", "richard@1234", "", "24");
-    // console.log("User 4 created successfully");
-
-    // const user5 = await customersData.createUser("Samuel", "jage", "samuel@gmail.com", "samueljage", "samuel@12", "", "20");
-    // console.log("User 5 created successfully");
-
-    // const user6 = await customersData.createUser("Raymond", "wright", "raymond@gmail.com", "raymondwright", "raymond@w123", "", "26");
-    // console.log("User 6 created successfully");
-
-    // const user7 = await customersData.createUser("nina", "dobrev", "nina@gmail.com", "ninadob", "ninadob@rev", "", "24");
-    // console.log("User 7 created successfully");
-
-    // const user8 = await customersData.createUser("shraddha", "khair", "shraddha@gmail.com", "shraddhak", "shraddha123kh", "", "25");
-    // console.log("User 8 created successfully");
-
-    // const user9 = await customersData.createUser("Savleen", "Kaur", "savleen@gmail.com", "savleenkaur", "savleenk@678kaur", "", "24");
-    // console.log("User 9 created successfully");
-
-    // const user10 = await customersData.createUser("Prateek", "Upereti", "prateek@gmail.com", "qawsed", "azsxdc", "", "24");
-    // console.log("User 10 created successfully");
-
-    // Creating Reviews
-    const review1 = await reviewsData.create(r1._id, user1._id, 'This is very good', 9)
-    console.log(review1)
-    console.log('Reviews1 added successfully')
-    // Creating Comment 
-    const comment1 = await commentsData.addComment(
-        user1._id,
-        review1._id,
-        "It is really good."
-    );
-    console.log(comment1)
-    console.log("Comment1 added successfully");
-
-    const comment2 = await commentsData.addComment(
-        user1._id,
-        review1._id,
-        "I liked it too."
-    );
-    console.log(comment2)
-    console.log("Comment2 added successfully");
     console.log('Finished seeding database');
+
     await connection.closeConnection();
 };
 
