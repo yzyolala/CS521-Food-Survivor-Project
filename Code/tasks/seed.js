f71ed41d38fd7484cf08a4f4195246ec762caae0
Zhenying Yu <zhenyingyu6@gmail.com>
2022-08-22 18:43:10 -0400

    Update seed.js

--- Code/tasks/seed.js
+++ Code/tasks/seed.js
@@ -10,34 +10,34 @@ async function main() {
     const db = await connection.dbConnection();
     await db.dropDatabase();
 
-    const r1 = await restaurantsData.create("Pizzeria", "732 Jefferson St, Hoboken, NJ", "07030", "Buffalo Chicken Pizza")
+    const r1 = await restaurantsData.create("Pizzeria", "732 Jefferson St, Hoboken, NJ", "07030", "Buffalo Chicken Pizza", '4')
     console.log("r1 has been added successfully")
 
-    const r2 = await restaurantsData.create("Del Frisco's Grille", "221 River St, Hoboken, NJ", "07030", "DEL'S DEVILED EGGS")
+    const r2 = await restaurantsData.create("Del Frisco's Grille", "221 River St, Hoboken, NJ", "07030", "DEL'S DEVILED EGGS", '5')
     console.log("r2 has been added successfully")
 
-    const r3 = await restaurantsData.create("Court Street", "61 6th St, Hoboken, NJ", "07030", "Smoked Simon")
+    const r3 = await restaurantsData.create("Court Street", "61 6th St, Hoboken, NJ", "07030", "Smoked Simon", '10')
     console.log("r3 has been added successfully")
 
-    const r4 = await restaurantsData.create("Muteki Ramen", "533 Washington St, Hoboken, NJ", "07030", "Shrimp Tempura")
+    const r4 = await restaurantsData.create("Muteki Ramen", "533 Washington St, Hoboken, NJ", "07030", "Shrimp Tempura", '7')
     console.log("r4 has been added successfully")
 
-    const r5 = await restaurantsData.create("Bareburger", "515 Washington St, Hoboken, NJ", "07030", "Blazin' Bacon")
+    const r5 = await restaurantsData.create("Bareburger", "515 Washington St, Hoboken, NJ", "07030", "Blazin' Bacon", '4')
     console.log("r5 has been added successfully")
 
-    const r6 = await restaurantsData.create("Battello", "502 Washington Blvd, Jersey City, NJ", "07310", "HAMACHI CRUDO")
+    const r6 = await restaurantsData.create("Battello", "502 Washington Blvd, Jersey City, NJ", "07310", "HAMACHI CRUDO", '4')
     console.log("r6 has been added successfully")
 
-    const r7 = await restaurantsData.create("Fire & Oak", "479 Washington Blvd, Jersey City, NJ", "07310", "NEW ENGLAND CLAM CHOWDER")
+    const r7 = await restaurantsData.create("Fire & Oak", "479 Washington Blvd, Jersey City, NJ", "07310", "NEW ENGLAND CLAM CHOWDER", '19')
     console.log("r7 has been added successfully")
 
-    const r8 = await restaurantsData.create("DOMODOMO", "200 Greene St, Jersey City, NJ", "07311", "White fish crudo")
+    const r8 = await restaurantsData.create("DOMODOMO", "200 Greene St, Jersey City, NJ", "07311", "White fish crudo", '30')
     console.log("r8 has been added successfully")
 
-    const r9 = await restaurantsData.create("Satis Bistro", "212 Washington St, Jersey City, NJ", "07302", "Brick Pressed Half Chicken")
+    const r9 = await restaurantsData.create("Satis Bistro", "212 Washington St, Jersey City, NJ", "07302", "Brick Pressed Half Chicken", '4')
     console.log("r9 has been added successfully")
 
-    const r10 = await restaurantsData.create("Luna", "279 Grove St, Jersey City, NJ", "07302", "Churro Ice Cream Sandwich")
+    const r10 = await restaurantsData.create("Luna", "279 Grove St, Jersey City, NJ", "07302", "Churro Ice Cream Sandwich", '7')
     console.log("r10 has been added successfully")
 
 
@@ -45,32 +45,32 @@ async function main() {
     const user1 = await customersData.createUser("John", "Kolh", "john@gmail.com", "johnk", "johnkolh", "", "24");
     console.log("User 1 created successfully");
 
-    const user2 = await customersData.createUser("Kate", "Ken", "kate@gmail.com", "katekk", " katekk123", "", "21");
+    const user2 = await customersData.createUser("Zhenying", "Yu", "zhenyingyu6@gmail.com", "yzyolala", "qawsed", "", "28");
     console.log("User 2 created successfully");
 
-    const user3 = await customersData.createUser("Tonny", "Keith", "tonny@gmail.com", "tonnykk", "tonnykeith@123", "", "23");
-    console.log("User 3 created successfully");
+    // const user3 = await customersData.createUser("Tonny", "Keith", "tonny@gmail.com", "tonnykk", "tonnykeith@123", "", "23");
+    // console.log("User 3 created successfully");
 
-    const user4 = await customersData.createUser("Richard", "Endrew", "richard@gmail.com", "richardend", "richard@1234", "", "24");
-    console.log("User 4 created successfully");
+    // const user4 = await customersData.createUser("Richard", "Endrew", "richard@gmail.com", "richardend", "richard@1234", "", "24");
+    // console.log("User 4 created successfully");
 
-    const user5 = await customersData.createUser("Samuel", "jage", "samuel@gmail.com", "samueljage", "samuel@12", "", "20");
-    console.log("User 5 created successfully");
+    // const user5 = await customersData.createUser("Samuel", "jage", "samuel@gmail.com", "samueljage", "samuel@12", "", "20");
+    // console.log("User 5 created successfully");
 
-    const user6 = await customersData.createUser("Raymond", "wright", "raymond@gmail.com", "raymondwright", "raymond@w123", "", "26");
-    console.log("User 6 created successfully");
+    // const user6 = await customersData.createUser("Raymond", "wright", "raymond@gmail.com", "raymondwright", "raymond@w123", "", "26");
+    // console.log("User 6 created successfully");
 
-    const user7 = await customersData.createUser("nina", "dobrev", "nina@gmail.com", "ninadob", "ninadob@rev", "", "24");
-    console.log("User 7 created successfully");
+    // const user7 = await customersData.createUser("nina", "dobrev", "nina@gmail.com", "ninadob", "ninadob@rev", "", "24");
+    // console.log("User 7 created successfully");
 
-    const user8 = await customersData.createUser("shraddha", "khair", "shraddha@gmail.com", "shraddhak", "shraddha123kh", "", "25");
-    console.log("User 8 created successfully");
+    // const user8 = await customersData.createUser("shraddha", "khair", "shraddha@gmail.com", "shraddhak", "shraddha123kh", "", "25");
+    // console.log("User 8 created successfully");
 
-    const user9 = await customersData.createUser("Savleen", "Kaur", "savleen@gmail.com", "savleenkaur", "savleenk@678kaur", "", "24");
-    console.log("User 9 created successfully");
+    // const user9 = await customersData.createUser("Savleen", "Kaur", "savleen@gmail.com", "savleenkaur", "savleenk@678kaur", "", "24");
+    // console.log("User 9 created successfully");
 
-    const user10 = await customersData.createUser("Prateek", "Upereti", "prateek@gmail.com", "qawsed", "azsxdc", "", "24");
-    console.log("User 10 created successfully");
+    // const user10 = await customersData.createUser("Prateek", "Upereti", "prateek@gmail.com", "qawsed", "azsxdc", "", "24");
+    // console.log("User 10 created successfully");
 
     // Creating Reviews
     const review1 = await reviewsData.create(r1._id, user1._id, 'This is very good', 9)
@@ -92,7 +92,6 @@ async function main() {
     );
     console.log(comment2)
     console.log("Comment2 added successfully");
-
     console.log('Finished seeding database');
     await connection.closeConnection();
 };
