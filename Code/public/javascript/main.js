9e5053319d9ba04a239bb24e3ede7e878a3bc878
Chaitanya Pawar <65788874+Chaitanyap22@users.noreply.github.com>
2022-08-24 11:25:06 -0400

    Added Credits and FAQs page
    Added Credits to referred third-party components, performed code cleanup
    
    Co-Authored-By: Zhenying Yu <zhenyingyu6@gmail.com>
    Co-Authored-By: DekeHe <98167199+DekeHe@users.noreply.github.com>
    Co-Authored-By: BARUN ROY <30745065+Roy-007@users.noreply.github.com>

--- Code/public/javascript/main.js
+++ Code/public/javascript/main.js
@@ -1,17 +1,5 @@
 (function ($) {
     "use strict";
-
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-
-
     // Initiate the wowjs
     new WOW().init();
 
@@ -45,33 +33,6 @@
     $('.back-to-top').click(function () {
         $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
-    });
-
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        margin: 25,
-        loop: true,
-        center: true,
-        dots: false,
-        nav: true,
-        navText: [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ],
-        responsive: {
-            0: {
-                items: 1
-            },
-            768: {
-                items: 2
-            },
-            992: {
-                items: 3
-            }
-        }
     });
 
 })(jQuery);
